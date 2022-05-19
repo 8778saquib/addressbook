167cef5d4c6e4490ce274c3c6dd852a5d5098b99
8778saquib <saquib.nawaz009@gmail.com>
2022-05-20 00:35:14 +0530

    addressbook

---
+++ addressbooks/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace addressbooks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welcome to address book program");
+        }
+    }
+}
