e7488ace5f93cf5a6122f79c394ac2e19d8fbf11
8778saquib <saquib.nawaz009@gmail.com>
2022-05-20 14:04:34 +0530

    createcontact

--- addressbooks/Program.cs
+++ addressbooks/Program.cs
@@ -7,6 +7,28 @@ namespace addressbooks
         static void Main(string[] args)
         {
             Console.WriteLine("welcome to address book program");
+
+            Console.WriteLine("Enter the choice");
+            Console.WriteLine(" 1. adding detail");
+
+            int choice = Convert.ToInt32(Console.ReadLine());
+
+            switch (choice)
+            {
+                case 1:
+                    createcontact a = new createcontact("saquib", "nawaz", "Warje", "chennai", "tamilnadu", "saquib.nawaz009@gmail.com", 274001, 8778225600);
+                    a.DisplayDetails();
+                    break;
+
+                default:
+                    Console.WriteLine("Please enter the correct choice");
+                    break;
+
+            }
+
         }
+
     }
 }
+
+
---
+++ addressbooks/createcontact.cs
@@ -0,0 +1,48 @@
+﻿using System;
+namespace addressbooks
+{
+    public class createcontact
+    {
+
+        //variable initialization
+
+        public string firstName;
+        public string lastName;
+        public string address;
+        public string city;
+        public string state;
+        public string email;
+        public int zip;
+        public long phoneNumber;
+
+        // parameterized construstor 
+
+        public createcontact(string firstName, string lastName, string address, string city, string state, string email, int zip, long phoneNumber)
+        {
+
+            //set values of class level variables from the constructor
+
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.address = address;
+            this.city = city;
+            this.state = state;
+            this.email = email;
+            this.zip = zip;
+            this.phoneNumber = phoneNumber;
+        }
+
+        public void DisplayDetails()
+        {
+            Console.WriteLine("First Name : " + this.firstName);
+            Console.WriteLine("Last Name : " + this.lastName);
+            Console.WriteLine("Address : " + this.address);
+            Console.WriteLine("City : " + this.city);
+            Console.WriteLine("State : " + this.state);
+            Console.WriteLine("Email id : " + this.email);
+            Console.WriteLine("Zip code : " + this.zip);
+            Console.WriteLine("Phone number : " + this.phoneNumber);
+        }
+
+    }
+}
