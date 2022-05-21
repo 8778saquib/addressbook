7ac5784e220f0d791b5aa0903e07f5acecdb5f76
8778saquib <saquib.nawaz009@gmail.com>
2022-05-21 15:25:34 +0530

    add or edit detail

--- addressbooks/Program.cs
+++ addressbooks/Program.cs
@@ -11,6 +11,7 @@ namespace addressbooks
             Console.WriteLine("Enter the choice");
             Console.WriteLine(" 1. adding detail");
             Console.WriteLine("2. Add new contact");
+            Console.WriteLine("3.edit add  detail");
 
             int choice = Convert.ToInt32(Console.ReadLine());
 
@@ -25,6 +26,23 @@ namespace addressbooks
                     person.AddingContactDetails();
                     person.DisplayDetails();
                     break;
+
+                case 3:
+                    Addcontact person2 = new Addcontact();
+                    person2.AddingContactDetails();
+                    person2.DisplayDetails();
+
+                    //Asking user if he/she wanted to edit the contact details or not
+                    Console.WriteLine("Edit contact details using name ? 1: Yes/ Other: No");
+                    Console.Write("Enter your choice : ");
+                    int choice3 = Convert.ToInt32(Console.ReadLine());
+                    if (choice3 == 1)
+                    {
+                        person2.EditContactDetails();
+                        person2.DisplayDetails();
+                    }
+                    break;
+
 
                 default:
                     Console.WriteLine("Please enter the correct choice");
